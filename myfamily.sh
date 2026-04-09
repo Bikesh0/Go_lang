03ac1eff5a1306b014af6a7af39acef56f46c228
Bikesh Shrestha <bikesh.shrestha@iMac-2.local>
2026-04-09 17:08:13 +0300

    family

--- myfamily.sh
+++ myfamily.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+#!/bin/bash
+
 if [ -z "$HERO_ID" ]; then
     echo "Please set the HERO_ID environment variable."
     exit 1
 fi
 
-
 curl -s "https://01.gritlab.ax/assets/superhero/all.json" | \
-jq -r --arg id "$HERO_ID" \
-  '.[] | select(.id == ($id | tonumber)) | .connections.relatives // empty'
+jq -r --arg id "$HERO_ID" '
+  .[] 
+  | select(.id == ($id | tonumber)) 
+  | .connections.relatives 
+  | if type=="array" then join(", ") else . end
+'
