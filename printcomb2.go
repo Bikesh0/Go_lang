db3d95d9ea2de2eff7f65576d974d4a7d6eeb8b4
Bikesh Shrestha <bikesh.shrestha@iMac-2.local>
2026-04-10 18:07:24 +0300

    w

--- printcomb2.go
+++ printcomb2.go
@@ -6,22 +6,53 @@ func PrintComb2() {
 	for a := 0; a <= 98; a++ {
 		for b := a + 1; b <= 99; b++ {
 
-			// print first number (a)
-			z01.PrintRune(rune('0' + a/10))
-			z01.PrintRune(rune('0' + a%10))
-
+			print2(a)
 			z01.PrintRune(' ')
+			print2(b)
 
-			// print second number (b)
-			z01.PrintRune(rune('0' + b/10))
-			z01.PrintRune(rune('0' + b%10))
-
-			// print separator except last
-			if a != 98 || b != 99 {
+			if !(a == 98 && b == 99) {
 				z01.PrintRune(',')
 				z01.PrintRune(' ')
 			}
 		}
 	}
 	z01.PrintRune('\n')
+}
+
+func print2(n int) {
+	printDigit(n / 10)
+	printDigit(n % 10)
+}
+
+func printDigit(d int) {
+	if d == 0 {
+		z01.PrintRune('0')
+	}
+	if d == 1 {
+		z01.PrintRune('1')
+	}
+	if d == 2 {
+		z01.PrintRune('2')
+	}
+	if d == 3 {
+		z01.PrintRune('3')
+	}
+	if d == 4 {
+		z01.PrintRune('4')
+	}
+	if d == 5 {
+		z01.PrintRune('5')
+	}
+	if d == 6 {
+		z01.PrintRune('6')
+	}
+	if d == 7 {
+		z01.PrintRune('7')
+	}
+	if d == 8 {
+		z01.PrintRune('8')
+	}
+	if d == 9 {
+		z01.PrintRune('9')
+	}
 }
