6569f14257580ad577b03b1e3fa72b6e39da3f7b
bshresth <bikesh.shr1@gmail.com>
2026-04-12 12:25:48 +0300

    w

---
+++ atoi.go
@@ -0,0 +1,25 @@
+package piscine
+
+func Atoi(s string) int {
+	result := 0
+	sign := 1
+
+	for i := 0; i < len(s); i++ {
+		// handle sign only at first position
+		if i == 0 && (s[i] == '-' || s[i] == '+') {
+			if s[i] == '-' {
+				sign = -1
+			}
+			continue
+		}
+
+		// reject anything not digit
+		if s[i] < '0' || s[i] > '9' {
+			return 0
+		}
+
+		result = result*10 + int(s[i]-'0')
+	}
+
+	return result * sign
+}
