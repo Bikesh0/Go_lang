532e5e933b1e00554c5034594c60a2ad7ef84b4f
bshresth <bikesh.shr1@gmail.com>
2026-04-15 11:31:13 +0300

    w

--- Quest2/printdigits/main.go
+++ Quest2/printdigits/main.go
@@ -3,8 +3,8 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	for i := 0; i <= 9; i++ {
-		z01.PrintRune(rune(i) + '0')
+	for i := '0'; i <= '9'; i++ {
+		z01.PrintRune(i)
 	}
 	z01.PrintRune('\n')
 }
