91d3b2e9d0534fb7b73ad5da5bab37e63acc6adf
bshresth <bikesh.shr1@gmail.com>
2026-04-15 11:56:21 +0300

    w

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -7,10 +7,13 @@ func IterativeFactorial(nb int) int {
 	if nb == 0 {
 		return 1
 	}
+
 	factorial := 1
 	for i := 1; i <= nb; i++ {
 		factorial *= i
-		return 0
+		if factorial < 0 {
+			return 0
+		}
 	}
 	return factorial
 }
