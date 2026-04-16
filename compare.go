778402ca76ec92c0d23daabb193a1d176d8e621f
Christopher Fremond <dev@01-edu.org>
2026-04-16 12:18:42 +0000

    Add compare.go

---
+++ compare.go
@@ -0,0 +1,23 @@
+package piscine
+
+func Compare(a, b string) int {
+	i := 0
+
+	for i < len(a) && i < len(b) {
+		if a[i] < b[i] {
+			return -1
+		}
+		if a[i] > b[i] {
+			return 1
+		}
+		i++
+	}
+
+	if len(a) < len(b) {
+		return -1
+	}
+	if len(a) > len(b) {
+		return 1
+	}
+	return 0
+}
