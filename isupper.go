75d04d665242df332e9b77463f83f2187d671623
bshresth <bikesh.shr1@gmail.com>
2026-04-16 16:38:21 +0300

    w

--- isupper.go
+++ isupper.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsUpper(s string) bool {
 	for i := 0; i < len(s); i++ {
-		if (s[i] < 'A' || s[i] > 'Z') && (s[i] < 'a' || s[i] > 'z') {
+		if s[i] < 'A' || s[i] > 'Z' {
 			return false
 		}
 	}
