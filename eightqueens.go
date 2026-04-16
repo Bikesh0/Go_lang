73320362abd10f71ac1cd5a35f96f64062d5019f
bshresth <bikesh.shr1@gmail.com>
2026-04-16 14:05:53 +0300

    w

--- eightqueens.go
+++ eightqueens.go
@@ -3,13 +3,13 @@ package piscine
 import "github.com/01-edu/z01"
 
 func EightQueens() {
-	var game [8]int
+	var board [8]int
 
 	var backtrack func(int)
 	backtrack = func(col int) {
 		if col == 8 {
 			for i := 0; i < 8; i++ {
-				z01.PrintRune(rune(game[i] + '1'))
+				z01.PrintRune(rune(board[i] + '1'))
 			}
 			z01.PrintRune('\n')
 			return
@@ -19,14 +19,18 @@ func EightQueens() {
 			valid := true
 
 			for i := 0; i < col; i++ {
-				if game[i] == row || game[i]-i == row-col || game[i]+i == row+col {
+				diff := board[i] - row
+				if diff < 0 {
+					diff = -diff
+				}
+				if board[i] == row || diff == col-i {
 					valid = false
 					break
 				}
 			}
 
 			if valid {
-				game[col] = row
+				board[col] = row
 				backtrack(col + 1)
 			}
 		}
