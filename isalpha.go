204aaa945d2c82a8dd03461fb1da419e738e29ba
bshresth <bikesh.shr1@gmail.com>
2026-04-16 16:44:51 +0300

    w

---
+++ isalpha.go
@@ -0,0 +1,14 @@
+package piscine
+
+func IsLower(s string) bool {
+	if s == "" {
+		return false
+	}
+
+	for i := 0; i < len(s); i++ {
+		if (s[i] < 'a' || s[i] > 'z') && (s[i] < 'A' || s[i] > 'Z') && (s[i] < '0' || s[i] > '9') {
+			return false
+		}
+	}
+	return true
+}
