7fb0f244b2656fb45bff5ce3a674f32799794cee
bshresth <bikesh.shr1@gmail.com>
2026-04-16 16:56:02 +0300

    w

---
+++ isnumeric.go
@@ -0,0 +1,14 @@
+package piscine
+
+func IsNumeric(s string) bool {
+	if s == "" {
+		return false
+	}
+
+	for i := 0; i < len(s); i++ {
+		if s[i] < '0' || s[i] > '9' {
+			return false
+		}
+	}
+	return true
+}
