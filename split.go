9c169db3ba74195f6d5bb582eb7590c58912a001
bshresth <bikesh.shr1@gmail.com>
2026-04-20 20:51:03 +0300

    W

---
+++ split.go
@@ -0,0 +1,29 @@
+package piscine
+
+// Split splits a string by a given separator and returns a slice of strings.
+func Split(s, sep string) []string {
+	var result []string
+
+	if sep == "" {
+		// If separator is empty, return the whole string as one element
+		return []string{s}
+	}
+
+	start := 0
+	for i := 0; i <= len(s)-len(sep); {
+		if s[i:i+len(sep)] == sep {
+			// Add substring before separator
+			result = append(result, s[start:i])
+			// Move past separator
+			i += len(sep)
+			start = i
+		} else {
+			i++
+		}
+	}
+
+	// Add remaining part
+	result = append(result, s[start:])
+
+	return result
+}
