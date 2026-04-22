41168a275c948f34b8818afc667614524c7744d7
bshresth <bikesh.shr1@gmail.com>
2026-04-22 19:36:16 +0300

    W

---
+++ quest/quest7/quest8/point/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"os"
+)
+
+func printString(s string) {
+	os.Stdout.Write([]byte(s))
+}
+
+func main() {
+	args := os.Args[1:]
+
+	// No argument
+	if len(args) == 0 {
+		printString("File name missing\n")
+		return
+	}
+
+	// More than one argument
+	if len(args) > 1 {
+		printString("Too many arguments\n")
+		return
+	}
+
+	// Exactly one argument
+	content, err := os.ReadFile(args[0])
+	if err != nil {
+		return
+	}
+
+	printString(string(content))
+}
