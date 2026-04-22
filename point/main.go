ad8b52fb6dfe55c72313736f9861ad6b42680dd3
bshresth <bikesh.shr1@gmail.com>
2026-04-22 18:53:09 +0300

    W

--- point/main.go
+++ point/main.go
@@ -32,3 +32,28 @@ func main() {
 	z01.PrintRune(points.y%10 + '0')
 	z01.PrintRune('\n')
 }
+package main
+
+import "github.com/01-edu/z01"
+
+type point struct {
+	x int
+	y int
+}
+
+func setPoint(ptr *point) {
+	ptr.x = 42
+	ptr.y = 21
+}
+
+func main() {
+	points := &point{}
+	setPoint(points)
+
+	z01.PrintRune('x'); z01.PrintRune(' '); z01.PrintRune('='); z01.PrintRune(' ')
+	z01.PrintRune(points.x/10 + '0'); z01.PrintRune(points.x%10 + '0')
+	z01.PrintRune(','); z01.PrintRune(' ')
+	z01.PrintRune('y'); z01.PrintRune(' '); z01.PrintRune('='); z01.PrintRune(' ')
+	z01.PrintRune(points.y/10 + '0'); z01.PrintRune(points.y%10 + '0')
+	z01.PrintRune('\n')
+}
