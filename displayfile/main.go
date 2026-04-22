edfe3b75a7ab90e2280a2b2d9d9fe6b99dc08dd4
bshresth <bikesh.shr1@gmail.com>
2026-04-22 19:40:41 +0300

    W

--- displayfile/main.go
+++ displayfile/main.go
@@ -5,75 +5,31 @@ import (
 )
 
 func printString(s string) {
-	os.Stdout.Write([]byte(s))
-}
-
-func atoi(s string) (int, bool) {
-	n := 0
 	for _, c := range s {
-		if c < '0' || c > '9' {
-			return 0, false
-		}
-		n = n*10 + int(c-'0')
+		os.Stdout.Write([]byte{byte(c)})
 	}
-	return n, true
 }
 
 func main() {
 	args := os.Args[1:]
 
-	if len(args) < 3 || args[0] != "-c" {
+	// No argument
+	if len(args) == 0 {
+		printString("File name missing\n")
 		return
 	}
 
-	n, ok := atoi(args[1])
-	if !ok || n < 0 {
+	// More than one argument
+	if len(args) > 1 {
+		printString("Too many arguments\n")
 		return
 	}
 
-	files := args[2:]
-	exitCode := 0
-
-	for i, name := range files {
-		file, err := os.Open(name)
-		if err != nil {
-			printString(err.Error() + "\n")
-			exitCode = 1
-			continue
-		}
-
-		info, err := file.Stat()
-		if err != nil {
-			file.Close()
-			exitCode = 1
-			continue
-		}
-
-		size := info.Size()
-		start := int64(0)
-
-		if int64(n) < size {
-			start = size - int64(n)
-		}
-
-		file.Seek(start, 0)
-
-		if len(files) > 1 {
-			if i > 0 {
-				printString("\n")
-			}
-			printString("==> " + name + " <==\n")
-		}
-
-		buf := make([]byte, n)
-		bytesRead, _ := file.Read(buf)
-
-		os.Stdout.Write(buf[:bytesRead])
-
-		file.Close()
+	// Exactly one argument
+	content, err := os.ReadFile(args[0])
+	if err != nil {
+		return
 	}
 
-	if exitCode != 0 {
-		os.Exit(1)
-	}
+	printString(string(content))
 }
