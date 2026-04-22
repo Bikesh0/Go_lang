da096894cf62584882028112cd73d25a574f023e
bshresth <bikesh.shr1@gmail.com>
2026-04-22 18:33:28 +0300

    W

--- point/main.go
+++ point/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "fmt"
+import "github.com/01-edu/z01"
 
 type point struct {
 	x int
@@ -12,10 +12,29 @@ func setPoint(ptr *point) {
 	ptr.y = 21
 }
 
+func printInt(n int) {
+	if n >= 10 {
+		printInt(n / 10)
+	}
+	z01.PrintRune(rune(n%10 + '0'))
+}
+
 func main() {
 	points := &point{}
 
 	setPoint(points)
 
-	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+	z01.PrintRune('x')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	printInt(points.x)
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('y')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	printInt(points.y)
+	z01.PrintRune('\n')
 }
