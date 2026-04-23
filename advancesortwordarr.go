3298f58a325224ba0dcfb9fcb1d99deaa10f4bfc
bshresth <bikesh.shr1@gmail.com>
2026-04-23 16:55:03 +0300

    W

--- advancesortwordarr.go
+++ advancesortwordarr.go
@@ -1,39 +1,17 @@
-package main
+package piscine
 
-import (
-	"fmt"
-)
-
-// Compare compares two strings using ASCII order
-func Compare(a, b string) int {
-	if a < b {
-		return -1
-	}
-	if a > b {
-		return 1
-	}
-	return 0
-}
-
-// AdvancedSortWordArr sorts using the comparison function f
-func AdvancedSortWordArr(a []string, f func(a, b string) int) {
+// SortWordArr sorts a slice of strings in ascending ASCII order.
+func SortWordArr(a []string) {
 	n := len(a)
 
-	// Bubble sort
+	// Bubble sort algorithm
 	for i := 0; i < n-1; i++ {
 		for j := 0; j < n-i-1; j++ {
-			// Use f to decide order
-			if f(a[j], a[j+1]) > 0 {
+			// Compare strings directly (ASCII order)
+			if a[j] > a[j+1] {
+				// Swap elements
 				a[j], a[j+1] = a[j+1], a[j]
 			}
 		}
 	}
-}
-
-func main() {
-	result := []string{"a", "A", "1", "b", "B", "2", "c", "C", "3"}
-
-	AdvancedSortWordArr(result, Compare)
-
-	fmt.Println(result)
 }
