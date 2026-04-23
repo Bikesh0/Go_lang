e543ca37f9074fb96ac4a5c09f44848b05d35038
bshresth <bikesh.shr1@gmail.com>
2026-04-23 15:03:18 +0300

    W

--- doop/main.go
+++ doop/main.go
@@ -4,11 +4,6 @@ import (
 	"os"
 )
 
-const (
-	maxInt = 9223372036854775807
-	minInt = -9223372036854775808
-)
-
 func main() {
 	if len(os.Args) != 4 {
 		return
@@ -18,12 +13,10 @@ func main() {
 	b, ok2 := Atoi(os.Args[3])
 	op := os.Args[2]
 
-	// invalid numbers
 	if !ok1 || !ok2 {
 		return
 	}
 
-	// division / modulo by zero
 	if op == "/" && b == 0 {
 		printStr("No division by 0\n")
 		return
@@ -33,52 +26,44 @@ func main() {
 		return
 	}
 
-	var result int
-	var ok bool = true
+	var res int64
 
 	switch op {
 	case "+":
-		if (b > 0 && a > maxInt-b) || (b < 0 && a < minInt-b) {
-			ok = false
-		} else {
-			result = a + b
-		}
+		res = a + b
 	case "-":
-		if (b < 0 && a > maxInt+b) || (b > 0 && a < minInt+b) {
-			ok = false
-		} else {
-			result = a - b
-		}
+		res = a - b
 	case "*":
-		if a != 0 && (b > maxInt/a || b < minInt/a) {
-			ok = false
-		} else {
-			result = a * b
-		}
+		res = a * b
 	case "/":
-		result = a / b
+		res = a / b
 	case "%":
-		result = a % b
+		res = a % b
 	default:
 		return
 	}
 
-	if !ok {
+	// check overflow for int
+	maxInt := int64(int(^uint(0) >> 1))
+	minInt := -maxInt - 1
+
+	if res > maxInt || res < minInt {
 		return
 	}
 
-	PrintNbr(result)
+	PrintNbr(int(res))
 	printStr("\n")
 }
 
-func Atoi(s string) (int, bool) {
+// Atoi returns int64 directly
+func Atoi(s string) (int64, bool) {
 	if len(s) == 0 {
 		return 0, false
 	}
 
-	sign := 1
+	sign := int64(1)
 	i := 0
-	var res int64 = 0
+	var res int64
 
 	if s[0] == '-' {
 		sign = -1
@@ -94,17 +79,9 @@ func Atoi(s string) (int, bool) {
 		}
 
 		res = res*10 + int64(s[i]-'0')
-
-		// overflow check during parsing
-		if sign == 1 && res > maxInt {
-			return 0, false
-		}
-		if sign == -1 && -res < minInt {
-			return 0, false
-		}
 	}
 
-	return int(res) * sign, true
+	return res * sign, true
 }
 
 func PrintNbr(n int) {
