9fdc73adb3a369561220a31f2aa4cb65195528fa
bshresth <bikesh.shr1@gmail.com>
2026-04-25 17:31:35 +0300

    W

---
+++ enigma.go
@@ -0,0 +1,15 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+
+	A := ***a
+	B := *b
+	C := *******c
+	D := ****d
+
+	***a := C
+	*******c := D
+	****d := B
+	*b := A
+
+}
