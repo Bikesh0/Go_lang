e9ca8be89e47e48caa239db22abc5bb25e228175
bshresth <bikesh.shr1@gmail.com>
2026-04-26 05:08:43 +0300

    fumpt

--- max.go
+++ max.go
@@ -1,12 +1,12 @@
 package piscine
 
 func Max(a []int) int {
-  highestValue:= 0
+	highestValue := 0
 
-  for i:= 0; i<len(a); i++ {
-    if a[i]>highestValue {
-      highestValue = a[i]
-    }
-  }
-  return highestValue
+	for i := 0; i < len(a); i++ {
+		if a[i] > highestValue {
+			highestValue = a[i]
+		}
+	}
+	return highestValue
 }
